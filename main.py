77d2169ee041cefe05a26e73eb98c93aacb8819c
maen1 <mk.maen93@gmail.com>
2018-11-29 15:32:11 +0000

    nah

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np 
 import pandas as pd 
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+#import matplotlib
+#matplotlib.use('TkAgg')
+#import matplotlib.pyplot as plt
 
 import keras
 from keras.optimizers import Adam
