27b27b288942e5265a3457e2b5e88feef1fdccd6
Maen1 <mk.maen93@gmail.com>
2018-12-02 23:24:20 +0800

    update demo

--- demo.py
+++ demo.py
@@ -1,16 +1,29 @@
-import numpy as np
 import cv2
+import numpy as np
+from PIL import Image
+from keras import models
 
-cap = cv2.VideoCapture(0)
+model = models.load_model('model_leNet.h5')
+video = cv2.VideoCapture(0)
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret==True:
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
+while True:
+        _, frame = video.read()
 
-cap.release()
-cv2.destroyAllWindows()
+        im = Image.fromarray(frame, 'RGB')
+
+        im = im.resize((28,28))
+        img_array = np.array(im)
+
+        
+        img_array = np.expand_dims(img_array, axis=0)
+
+        prediction = int(model.predict(img_array)[0][0])
+
+        if prediction == 0:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow("Capturing", frame)
+        key=cv2.waitKey(1)
+        if key == ord('q'):
+                break
+video.release()
