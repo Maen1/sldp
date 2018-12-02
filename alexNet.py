a615a49b1f0cd9ddd1c8faadce078d7ccd32a55b
Maen1 <mk.maen93@gmail.com>
2018-12-02 22:19:35 +0800

    wip

--- alexNet.py
+++ alexNet.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np 
 import pandas as pd 
-import matplotlib
-matplotlib.use('TkAgg')
+# import matplotlib
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 import keras
