6bf315e4f0b05e145b1f68adadb4a304c4fbca9f
Maen1 <mk.maen93@gmail.com>
2019-03-06 03:05:35 +0800

    upate object test

--- pvoc.py
+++ pvoc.py
@@ -1,14 +1,16 @@
 
 from  pascal_voc_writer import Writer
 import os
-path = "../tf-dataset/test/"
+# path = "../tf-dataset/test/"
+path = "../asl-alphabet/asl_alphabet_test/"
 
 for filename in os.listdir(path):
     if filename.endswith("jpg"):
         writer = Writer(path, 200,200)
 
-        writer.addObject('a',0,0,200,200)
         name = os.path.splitext(filename)[0]
+        object = name[0]
+        writer.addObject(object,0,0,200,200)
         # print(path + name+'.xml')
         writer.save(path + name+'.xml')
 
