7125b602cce5b018f5898cb1fb1666d9dc70b979
Maen1 <mk.maen93@gmail.com>
2019-03-06 01:13:53 +0800

    add file name without extension

--- pvoc.py
+++ pvoc.py
@@ -7,9 +7,8 @@ path = "../asl-alphabet/asl_alphabet_train/B/"
 # writer.addObject('a',0,0,200,200)
 
 # writer.save('./a.xml')
-ctr=0;
 for filename in os.listdir(path):
     if filename.endswith("jpg"): 
         # Your code comes here such as
-        print(filename)
-    print(ctr)
+        name = os.path.splitext(filename)[0]
+        print(name)
