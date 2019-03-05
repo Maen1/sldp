91634ed69e4e52f5cbdc5c7bc0edb4bcf9e7cb77
Maen1 <mk.maen93@gmail.com>
2019-03-06 01:22:35 +0800

    update pvoc

--- pvoc.py
+++ pvoc.py
@@ -2,13 +2,12 @@
 from  pascal_voc_writer import Writer
 import os
 path = "../asl-alphabet/asl_alphabet_train/B/"
-# writer = Writer(path, 200,200)
 
-# writer.addObject('a',0,0,200,200)
-
-# writer.save('./a.xml')
 for filename in os.listdir(path):
-    if filename.endswith("jpg"): 
-        # Your code comes here such as
+    if filename.endswith("jpg"):
+        writer = Writer(path, 200,200)
+
+        writer.addObject('a',0,0,200,200)
         name = os.path.splitext(filename)[0]
-        print(name)
+        print(path + name+'.xml')
+        # writer.save(path + name+'.xml')
